0d219d45501893495d1015485c518833623f0e2b
Stanislav Vodolagin <ximtech@inbox.lv>
2024-07-12 19:23:19 +0300

    Dummy test try

---
+++ src/test/groovy/net/btcbit/homework/PianoHandlerSpec.groovy
@@ -0,0 +1,13 @@
+package net.btcbit.homework
+
+import spock.lang.Specification
+
+class PianoHandlerSpec extends Specification {
+
+
+    def 'dummy test'() {
+        expect:
+        1 == 1
+    }
+    
+}
